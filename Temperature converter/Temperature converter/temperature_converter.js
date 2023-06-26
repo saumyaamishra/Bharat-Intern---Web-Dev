e6b74aaa4a1ff8bbd9f22742e6157f88b8c176c8
saumyaamishra <122546558+saumyaamishra@users.noreply.github.com>
2023-06-26 14:57:21 +0530

    Add files via upload

---
+++ Temperature converter/Temperature converter/temperature_converter.js
@@ -0,0 +1,42 @@
+function CalcTemp() {
+    var temp1 = Number(document.getElementById("temp1").value);
+    //calculate
+    var temp1Unit = document.getElementById("temp1Unit").value; 
+    var temp2Unit = document.getElementById("temp2Unit").value;
+    var res = Number(document.getElementById("result").value);
+    var celTemp = Number(celciusTemp(temp1Unit, temp1));
+    res = Number(finalTemp(temp2Unit, celTemp)).toFixed(3);
+    document.getElementById("result").innerHTML = res;
+}
+
+function celciusTemp(Unit_Temp1, temp1) {
+    var celTemp = 0;
+    switch (Unit_Temp1) {
+        case "celcius":
+            celTemp = temp1;
+            break;
+        case "farhen":
+            celTemp = (temp1 - 32) * 5/9;
+            break;
+        case "kelvin":
+            celTemp = temp1 - 273.15;
+            break;
+    }
+    return celTemp;
+}
+
+function finalTemp(Unit_Temp2, celTemp) {
+    var result = 0;
+    switch (Unit_Temp2) {
+        case "celcius":
+            result = celTemp;
+            break;
+        case "farhen":
+            result = (celTemp * 1.8) + 32;
+            break;
+        case "kelvin":
+            result = celTemp + 273.15;
+            break;
+    } 
+    return result;
+}
